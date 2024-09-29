4994d5b19af08d4e4678a524b4cd30ef40111535
brlcxc <lohmanbishop@gmail.com>
2024-09-29 14:44:15 -0500

    fixing comments

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,17 +1,14 @@
 from django.db import models
-# from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
 
-# note: use uuid for unique ID
+# TODO: switch sequential ID to uuid
 
+# customer UserManager is used since we want to use email in place of username
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
     def _create_user(self, email, password, **extra_fields):
-        """
-        Creates and saves a User with the given email and password.
-        """
         if not email:
             raise ValueError('The given email must be set')
         email = self.normalize_email(email)
@@ -24,11 +21,13 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
+# AbstractUser contains all fields of User but has the ability to be extended off
 class User(AbstractUser):
     USERNAME_FIELD = 'email'
-    email = models.EmailField(('email address'), unique=True) # changes email to unique and blank to false
+    email = models.EmailField(('email address'), unique=True)
     objects = UserManager()
 
+    # changes the default username field from username to email on registration
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     
@@ -36,7 +35,7 @@ class User(AbstractUser):
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    # auto autotimatically populates on creation
+    # auto automatically populates field on creation
     created_at = models.DateTimeField(auto_now_add=True)
     # foreign key here links User to the collection of Notes - each user can have many notes
     # one to many
