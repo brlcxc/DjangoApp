df374c1085c73321d6a7c139e652e5c9a41e68a6
Jacob Ford <90845996+jfordnull@users.noreply.github.com>
2024-10-07 13:31:31 -0500

    Rehaul project structure for React

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen text-4xl">
+        <h1>Hello world!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,18 @@
+import react from '@vitejs/plugin-react'
+
 export default {
-	root: 'src/',
-	publicDir: '../static/',
-	server:
-	{
-		host: true,	
-		open: true
-	},
-	build:
+  root: 'src/',
+  publicDir: '../public/',
+  plugins: [react()],
+  server: {
+    host: true,
+    open: true
+  },
+  build:
 	{
 		outDir: '../dist',	
 		emptyOutDir: true,
 		sourcemap: true
 	
-	},
-};
+	}
+}
