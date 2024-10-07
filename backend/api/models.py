fed3deaa93df54800bca9eddc2da0f5ef4174fa0
brlcxc <lohmanbishop@gmail.com>
2024-10-06 22:39:25 -0500

    start of new models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
+from uuid import uuid4
 
 # TODO: switch sequential ID to uuid
 
 # customer UserManager is used since we want to use email in place of username
+# This overrides existing actions which will not be needed for other models
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
@@ -17,25 +19,38 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-    def create_user(self, email, password=None, **extra_fields):
+    # password = None does not seem necessary 
+    def create_user(self, email, password, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
         return self._create_user(email, password, **extra_fields)
 
 # AbstractUser contains all fields of User but has the ability to be extended off
 class User(AbstractUser):
     # Removing the username field from the model
+    # Note: could I technically set any default field I dont want to none?
     username = None
 
     # TODO remove null from display name
+    #Additional User fields
     display_name = models.CharField(max_length=100, null=True)
-
-    USERNAME_FIELD = 'email'
+    user_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     email = models.EmailField(('email address'), unique=True)
+    user_verified = models.BooleanField(default=False)
+
     objects = UserManager()
 
     # changes the default username field from username to email on registration
+    # Authentication settings?
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['display_name']
     
 # note: serializer needs to be made for this
 class Note(models.Model):
@@ -50,3 +65,28 @@ class Note(models.Model):
 
     def __str__(self):
         return self.title
+
+class Group(models.Model):
+    group_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    group_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    group_owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+class Transaction(models.Model):
+    transaction_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+    class Meta:
+        ordering = ['published_date']  # Orders by `published_date` ascending
+
+    def __str__(self):
+        return self.title
+
+class Invite(models.Model):
+    invite_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+# class GroupMembers(models.Model):
+#     pass
+
+# class InviteRecipients(models.Model):
+#     pass
