906a7e1541527bc2164b64b5c6d45b6a6d167789
Jacob Ford <90845996+jfordnull@users.noreply.github.com>
2024-10-07 22:14:34 -0500

    Update temp login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,9 @@
 function Login() {
-    return <div>Login</div>;
+    return(
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-4xl">Please Login</h1>
+        </div>
+    )
 }
 
 export default Login;
