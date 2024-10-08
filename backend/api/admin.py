bd94ac5a26d048ac37415aa0ff6e01a73ba4bce8
brlcxc <lohmanbishop@gmail.com>
2024-10-08 17:06:37 -0500

    adding other models to admin page

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,14 +1,27 @@
 from django.contrib import admin
 # from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import User, Group, Transaction, Invite
 
 # TODO fix this so that an admin user can properly login
 # the default UserAdmin needs to be overridden since username is no longer needed
+
+#add user currently doesnt work
 class UserAdmin(BaseUserAdmin):
     # Use email for ordering instead of username
+    list_display = ('email', 'display_name', 'id', 'user_verified', 'is_staff')
     ordering = ['email']
 
-admin.site.register(User, UserAdmin)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('group_name', 'group_owner_id', 'description')
 
-# Register your models here.
+class TransactionAdmin(admin.ModelAdmin):
+    pass
+
+class InviteAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Invite, InviteAdmin)
