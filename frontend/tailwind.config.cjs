58e4f421d3ddd04548f3319fd0740b0732a485ce
Sarkovos <mastersarkovos@gmail.com>
2024-10-12 11:12:32 -0500

    Initial commit with new FE login page
    Modified tailwind config for extra utility, as well as index.css

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,10 +1,30 @@
-export default {
-    content: [
-      "./src/index.html",
-      "./src/**/*.{js,jsx,}",
-    ],
-    theme: {
-      extend: {},
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        archivo: ["Archivo", "sans-serif"]
+      },
+      colors: {
+        "pale-purple": "#F1E3F3",
+        "periwinkle": "#C2BBF0",
+        "amaranth-pink": "#F698BB",
+        "deep-sky-blue": "#62BFED",
+        "dodger-blue": "#3590F3",
+      },
+      keyframes: {
+        /* Defines a custom keyframe animation named gradient*/
+        gradient: { 
+          "0%": { backgroundPosition: "0% 50%" },
+          "100%": { backgroundPosition: "100% 50%" },
+        },
+      },
+      /* Creates a Tailwind utility class called animate-gradient*/
+      animation: {
+        gradient: "gradient 6s linear infinite", 
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
