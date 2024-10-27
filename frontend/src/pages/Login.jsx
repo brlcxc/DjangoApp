3f8a05a0c11475d8efd520211353e228aed41090
Jacob Ford <90845996+jfordnull@users.noreply.github.com>
2024-10-27 01:44:44 -0500

    Add logo to login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,10 +2,9 @@ import LoginForm from "../components/LoginForm";
 
 function Login() {
     return(
-        <div className="grid h-screen w-full">
-            <div className="bg-custom-gradient animate-gradient flex flex-col justify-center p">
-                <LoginForm/>
-            </div>
+        <div className="bg-custom-gradient animate-gradient flex flex-col justify-center items-center gap-2 h-screen">
+            <img className="h-1/3 w-auto" src="/Holofund.png"></img>
+            <LoginForm/>
         </div>
     )
 }
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,9 @@
 function NotFound() {
-    return <>404</>
+    return(
+        <div className="bg-custom-gradient animate-gradient flex flex-col justify-center items-center h-screen">
+            <h1 className="text-9xl">404. Page Not Found</h1>
+        </div>
+    )
 }
 
 export default NotFound;
