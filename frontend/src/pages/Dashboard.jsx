a8e105aa857e24c88fa3a6264caa0ca25eb04888
Jacob Ford <90845996+jfordnull@users.noreply.github.com>
2024-10-27 01:27:30 -0500

    Add display name retrieval to dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import api from "../api";
+import { ACCESS_TOKEN } from "../constants";
+
 function Dashboard() {
-    return <>Home</>
+    const [userName, setUserName] = useState("");
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+          try {
+            const token = localStorage.getItem(ACCESS_TOKEN);
+    
+            const response = await api.get('api/users/me/', {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            });
+    
+            setUserName(response.data.display_name);
+          } catch (error) {
+            console.error('Error fetching user data:', error);
+          }
+        };
+        fetchUserData();
+      }, []);
+
+    return(
+        <div>
+            <h1>Welcome, {userName}!</h1>
+        </div>
+    )
 }
 
 export default Dashboard;
