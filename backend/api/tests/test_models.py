6bdbf4ee7187e7985922d0c7f255648b06e74813
brlcxc <lohmanbishop@gmail.com>
2024-10-31 11:57:32 -0500

    adding empty test files

--- backend/api/tests/test_models.py
+++ backend/api/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import User, Group, Transaction, Invite
+from ..models import User, Group, Transaction, Invite
 from uuid import uuid4
 
 class UserModelTests(TestCase):
