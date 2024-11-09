50aaccaa60d3deb1aad3cb785a4f0aff3d40c47f
brlcxc <lohmanbishop@gmail.com>
2024-11-09 13:17:06 -0600

    add logout functionality

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,15 @@
 import { FaHouse, FaGear, FaUser, FaUserGroup, FaRegCalendarPlus, FaChartLine, FaDollarSign, FaDoorOpen } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 function NavBar({setActivePage}){
     const iconSize = "36";
+    const navigate = useNavigate();
+
+    //Note: I am unsure if there is maybe a better way to link back to the logout route
+    const handleLogout = () => {
+      localStorage.clear(); // Clear any stored data
+      navigate("/logout");  // Redirect to the logout route
+    };
 
     return(
         <div className="top-0 left-0 h-screen w-24 flex flex-col bg-dodger-blue">
@@ -12,7 +20,7 @@ function NavBar({setActivePage}){
             <NavBarIcon icon={<FaUserGroup size={iconSize} />} text="Groups" onClick={() => setActivePage("Groups")}/>
             <NavBarIcon icon={<FaUser size={iconSize} />} text="Profile" onClick={() => setActivePage("User")}/>
             <NavBarIcon icon={<FaGear size={iconSize} />} text="Settings" onClick={() => setActivePage("Settings")}/>
-            <NavBarIcon icon={<FaDoorOpen size={iconSize} />} text="Logout" onClick={() => Logout()}/>
+            <NavBarIcon icon={<FaDoorOpen size={iconSize} />} text="Logout" onClick={handleLogout}/>
         </div>
     )
 }
