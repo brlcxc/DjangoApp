c0c26f3bd3639b83b9d263a4e98cc40d1fe3ea3b
brlcxc <lohmanbishop@gmail.com>
2024-11-09 11:35:25 -0600

    improving display and trying to integrate context

--- frontend/src/components/GroupMembership.jsx
+++ frontend/src/components/GroupMembership.jsx
@@ -1,37 +1,37 @@
 import React, { useEffect, useContext, useCallback, useState } from "react";
-import { GroupContext } from "../context/GroupContext"; 
+import { GroupContext } from "../context/GroupContext";
 
 const GroupRow = React.memo(({ group, isChecked, onCheckChange }) => (
-    <div className="flex items-center py-3 pl-3 border-b hover:bg-gray-100 transition text-black">
-    <input
-      type="checkbox"
-      className="mr-3"
-    />
+  <div className="flex items-center py-3 pl-3 border-b hover:bg-gray-100 transition text-black">
+    <input type="checkbox" className="mr-3" />
     <div className="flex-1">{group.group_name}</div>
     <div className="flex-1">{group.description}</div>
   </div>
-  ));
-
+));
 
 const GroupMembership = () => {
-    const { groups, loading, error } = useContext(GroupContext);
+  const { groups, loading, error } = useContext(GroupContext);
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error.message}</div>;
-  
-    const uuid = localStorage.getItem('USER_ID')
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
-    //I might need to start playing around with shared names and stuff
-    //also idk if I can even get the user id from front end
-    const filteredGroups = groups?.filter(
-        (group) => group.group_owner_id !== uuid
-      );
+  const uuid = localStorage.getItem("USER_ID");
 
-    console.log(uuid)
-    return (
-      <div>
-        <h1 className="text-2xl font-bold mb-5 text-black">Shared With Me</h1>
-        <div className="overflow-y-auto max-h-[350px]">
+  //I might need to start playing around with shared names and stuff
+  //also idk if I can even get the user id from front end
+  const filteredGroups = groups?.filter(
+    (group) => group.group_owner_id !== uuid
+  );
+
+  console.log(uuid);
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-5 text-black">Shared With Me</h1>
+      <div className="grid grid-cols-2 py-3 pl-3 border-b font-semibold text-left bg-dodger-blue text-white">
+        <div>Group</div>
+        <div>Description</div>
+      </div>
+      <div className="overflow-y-auto max-h-[350px]">
         {filteredGroups && filteredGroups.length > 0 ? (
           filteredGroups.map((group) => (
             <GroupRow key={group.group_id} group={group} />
@@ -41,9 +41,9 @@ const GroupMembership = () => {
         )}
       </div>
     </div>
-    );
-  };
-  
-  export default GroupMembership;
+  );
+};
 
-  //I need someway to make sure only one is selected between the two groups
+export default GroupMembership;
+
+//I need someway to make sure only one is selected between the two groups
--- frontend/src/pages/Groups.jsx
+++ frontend/src/pages/Groups.jsx
@@ -3,28 +3,28 @@ import OwnedGroups from "../components/OwnedGroups";
 import GroupMembership from "../components/GroupMembership";
 import GroupModify from "../components/GroupModify";
 
-function Groups(){
-    const style = "bg-white p-8 rounded-xl shadow-lg";
-    return (
-      <div className="grid grid-cols-2 gap-8 size-full p-8 bg-custom-gradient animate-gradient">
-        <div className="grid grid-rows-2 gap-8">
-          <div className={`${style}`}>
-            <OwnedGroups />
-          </div>
-          <div className={`${style}`}>
-          <GroupMembership />
-          </div>
+function Groups() {
+  const style = "bg-white p-8 rounded-xl shadow-lg";
+  return (
+    <div className="grid grid-cols-2 gap-8 size-full p-8 bg-custom-gradient animate-gradient">
+      <div className="grid grid-rows-2 gap-8">
+        <div className={`${style}`}>
+          <OwnedGroups />
         </div>
-        <div className="grid grid-rows-2 gap-8">
-          <div className={`${style}`}>
-          <GroupAdd />
-          </div>
-          <div className={`${style}`}>
-          <GroupModify />
-          </div>
+        <div className={`${style}`}>
+          <GroupMembership />
         </div>
       </div>
-    );
-  }
+      <div className="grid grid-rows-2 gap-8">
+        <div className={`${style}`}>
+          <GroupModify />
+        </div>
+        <div className={`${style}`}>
+          <GroupAdd />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Groups;
