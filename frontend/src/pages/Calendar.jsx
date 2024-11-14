958824170318ada393b1d7a5c6ff499336ca6ba9
brlcxc <brlcxc@umsystem.edu>
2024-11-13 23:08:47 -0600

    added border

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -2,13 +2,15 @@ import Month from "../components/Month";
 import { getMonth } from "../components/util";
 import { useState } from "react";
 
-function Calendar(){
-    const [currentMonth, setCurrentMonth] = useState(getMonth());
-    return(
-        <div className="w-full h-full flex">
-            <Month month={currentMonth}/>
-        </div>
-    )
+function Calendar() {
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+  return (
+    <div className="size-full p-8 bg-custom-gradient animate-gradient">
+      <div className="size-full flex bg-white">
+        <Month month={currentMonth} />
+      </div>
+    </div>
+  );
 }
 
 export default Calendar;
