5ba2f3b5d2d321496712552a1df9d3dcac0cec4a
brlcxc <brlcxc@umsystem.edu>
2024-11-14 13:03:59 -0600

    altering list format

--- frontend/src/components/GroupModify.jsx
+++ frontend/src/components/GroupModify.jsx
@@ -1,4 +1,4 @@
-import { useSelectedGroup } from '../context/GroupModifyContext'; // Adjust the path as necessary
+import { useSelectedGroup } from "../context/GroupModifyContext"; // Adjust the path as necessary
 
 const GroupModify = ({ groups = [], onDelete }) => {
   const { selectedGroup } = useSelectedGroup();
@@ -10,7 +10,11 @@ const GroupModify = ({ groups = [], onDelete }) => {
   console.log(selectedGroup);
 
   const handleDelete = () => {
-    if (window.confirm(`Are you sure you want to delete the group: ${selectedGroup.group_name}?`)) {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the group: ${selectedGroup.group_name}?`
+      )
+    ) {
       onDelete(selectedGroup.group_id);
     }
   };
@@ -20,28 +24,33 @@ const GroupModify = ({ groups = [], onDelete }) => {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold text-black">{selectedGroup.group_name}</h1>
+      <h1 className="text-2xl font-bold text-black">
+        {selectedGroup.group_name}
+      </h1>
       <p className="text-gray-600 text-xl mb-4">{selectedGroup.description}</p>
       <p className="text-gray-700 font-semibold text-xl mb-4">
-        Owner: <span className='font-normal'>{selectedGroup.owner_name}</span> 
+        Owner: <span className="font-normal">{selectedGroup.owner_name}</span>
         <span className="text-gray-500">({selectedGroup.owner_email})</span>
       </p>
-      <div className="w-[40%] py-3 pl-3 border-b font-semibold text-left bg-dodger-blue text-white">
+      <div className="py-3 pl-3 border-b font-semibold text-left bg-dodger-blue text-white">
         Members
       </div>
-      <ul className="list-disc list-inside mb-4">
-        {selectedGroup.members.map((member) => (
-          <li key={member.id} className="text-gray-700">
-            {member.display_name} <span className="text-gray-500">({member.email})</span>
-          </li>
-        ))}
-      </ul>
-      
+      <div className="py-3 pl-2 border-b hover:bg-gray-100 transition text-black">
+        <ul className="list-disc list-inside">
+          {selectedGroup.members.map((member) => (
+            <li key={member.id} className="text-gray-700">
+              {member.display_name}{" "}
+              <span className="text-gray-500">({member.email})</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+
       <button
         className="bg-coral hover:bg-deep-coral text-white font-semibold py-2 px-4 rounded shadow"
         onClick={handleDelete}
       >
-        {isOwner ? 'Delete Group' : 'Leave Group'}
+        {isOwner ? "Delete Group" : "Leave Group"}
       </button>
     </div>
   );
