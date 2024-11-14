7f88a950ebf4eb9a262d13d1bca1f50a6782e881
brlcxc <brlcxc@umsystem.edu>
2024-11-14 10:46:32 -0600

    better padding

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -6,8 +6,10 @@ function Calendar() {
   const [currentMonth, setCurrentMonth] = useState(getMonth());
   return (
     <div className="size-full p-8 bg-custom-gradient animate-gradient">
-      <div className="size-full flex bg-white">
-        <Month month={currentMonth} />
+      <div className="size-full flex bg-white rounded-xl">
+        <div className="size-full flex p-4">
+          <Month month={currentMonth} />
+        </div>
       </div>
     </div>
   );
