2ca89644e99d7c44271fc70e8ae77d2a4ef1a65e
brlcxc <brlcxc@umsystem.edu>
2024-11-13 23:24:02 -0600

    properly displays group name and email

--- frontend/src/components/GroupModify.jsx
+++ frontend/src/components/GroupModify.jsx
@@ -18,9 +18,9 @@ const GroupModify = ({ groups = [], onDelete }) => {
 //Maybe it would be good to have an inner white panel?
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-5 text-black">{selectedGroup.group_name}</h1>
+      <h1 className="text-2xl font-bold text-black">{selectedGroup.group_name}</h1>
       <p className="text-gray-600 text-xl mb-4">{selectedGroup.description}</p>
-      <p className="text-gray-600 text-xl mb-4">Owner: {selectedGroup.group_owner_id}</p>
+      <p className="text-gray-700 font-semibold text-xl mb-4">Owner: <span className='font-normal'>{selectedGroup.owner_name}</span> <span className="text-gray-500">({selectedGroup.owner_email})</span></p>
       <h3 className="text-lg font-semibold text-gray-700 mb-2">Members:</h3>
       <ul className="list-disc list-inside mb-4">
         {selectedGroup.members.map((member) => (
