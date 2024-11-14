3828000ae7d5750e00555cdc34001a440dd33a2d
brlcxc <brlcxc@umsystem.edu>
2024-11-14 14:26:18 -0600

    more features added

--- frontend/src/components/GroupModify.jsx
+++ frontend/src/components/GroupModify.jsx
@@ -33,12 +33,17 @@ const GroupModify = ({ groups = [], onDelete }) => {
         <span className="text-gray-500">({selectedGroup.owner_email})</span>
       </p>
       <div className="py-3 pl-3 border-b font-semibold text-left bg-dodger-blue text-white">
-        Members
+        Current Members
       </div>
       <div className="py-3 pl-2 border-b hover:bg-gray-100 transition text-black">
-        <ul className="list-disc list-inside">
+        <ul>
           {selectedGroup.members.map((member) => (
-            <li key={member.id} className="text-gray-700">
+            <li key={member.id} className="text-gray-700 flex items-center">
+              {isOwner && (
+                <button className="font-bold text-white text-l bg-coral mr-3 size-5 rounded p-1 hover:bg-deep-coral focus:outline-none">
+                  -
+                </button>
+              )}
               {member.display_name}{" "}
               <span className="text-gray-500">({member.email})</span>
             </li>
@@ -52,8 +57,28 @@ const GroupModify = ({ groups = [], onDelete }) => {
       >
         {isOwner ? "Delete Group" : "Leave Group"}
       </button>
+      <button className="bg-green-300 hover:bg-green-400 text-white font-semibold py-2 px-4 rounded shadow">
+        Invite
+      </button>
     </div>
   );
 };
 
 export default GroupModify;
+
+//I need the search user add user thing here too     add members section    invite button selected members ection
+//invite message too
+
+//still need the edit ability
+//maybe - button on list of users
+
+// <button
+// onClick={() => handleRemoveSituation(index)}
+// className="font-bold text-white bg-coral size-9 rounded p-1 hover:bg-deep-coral focus:outline-none"
+// >
+// &times;
+// </button>
+
+//maybe way to differentia between between recently added and new?
+
+//maybe change size of colums?
