85e9ff5ef6169e86bd8dfc338ae5fe26b6523e46
brlcxc <brlcxc@umsystem.edu>
2024-11-16 21:55:23 -0600

    mobile styling added

--- frontend/src/pages/Groups.jsx
+++ frontend/src/pages/Groups.jsx
@@ -6,24 +6,23 @@ import { GroupModifyProvider } from "../context/GroupModifyContext";
 
 function Groups() {
   const style = "bg-white p-8 rounded-xl shadow-lg";
+
   return (
     <GroupModifyProvider>
-      <div className="grid grid-cols-2 gap-8 size-full p-8 bg-custom-gradient animate-gradient">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-8 bg-custom-gradient min-h-screen">
         <div className="flex flex-col gap-8">
-          {" "}
-          <div className={`${style} h-[70%]`}>
+          <div className={`${style} h-[70%] md:h-[70%]`}>
             <GroupModify />
           </div>
-          <div className={`${style} h-[26%]`}>
+          <div className={`${style} h-[26%] md:h-[26%]`}>
             <OwnedGroups />
           </div>
         </div>
         <div className="flex flex-col gap-8">
-          {" "}
-          <div className={`${style} h-[70%]`}>
+          <div className={`${style} h-[70%] md:h-[70%]`}>
             <GroupAdd />
           </div>
-          <div className={`${style} h-[26%]`}>
+          <div className={`${style} h-[26%] md:h-[26%]`}>
             <GroupMembership />
           </div>
         </div>
