b9d71effc9201d63f7ff51062a44c25142549460
brlcxc <brlcxc@umsystem.edu>
2024-11-19 12:20:43 -0600

    H issue resolved

--- frontend/src/pages/AI.jsx
+++ frontend/src/pages/AI.jsx
@@ -2,7 +2,6 @@ import LLMInterface from "../components/LLMInterface";
 
 function AnalyticsPage(){
     return(
-        //For whatever reason the H height is improper
         <div className="p-8 flex items-center justify-center bg-custom-gradient animate-gradient min-h-screen">
             <LLMInterface />
         </div>
